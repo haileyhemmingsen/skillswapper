b587dd7a213e53df1a4df820ea9eb0cafd40160c
Bradly-H <149456135+Bradly-H@users.noreply.github.com>
2024-10-08 12:55:39 -0700

    check-account endpoint works, upon login, check moves through and onto auth when is correct

--- react-login/auth-server/endpoints/check_account.js
+++ react-login/auth-server/endpoints/check_account.js
@@ -11,14 +11,26 @@ function check_account(req, res) {
         }
 
         let db_content = JSON.parse(jsonData);
+        console.log("database content:");
+        console.log(db_content);
         let user_exists = db_content.users.find((user) => {
-            email === user.email;
+            // console.log(email === user.email);
+            return email === user.email;
         });
+        // console.log("about to say if user exists");
+        // if(user_exists) {
+        //     console.log('success');
+        // }
+        // else {
+        //     console.log('failure');
+        // }
+        // console.log('further testing');
+        // console.log(!!user_exists);
         // console.log(user)
         
         res.status(200).json({
-          status: user_exists ? 'User exists' : 'User does not exist',
-          userExists: user_exists,
+          status: !!user_exists ? 'User exists' : 'User does not exist',
+          userExists: !!user_exists,
         })
     });
 
