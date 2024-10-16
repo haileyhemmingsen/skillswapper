76d6a42af4c5f4377a998fdfa25bdd9543889953
Christian Wylie <cwylie@ucsc.edu>
2024-10-16 16:19:14 -0700

    All components to get the backend working

---
+++ react-login/auth-server/services/server.ts
@@ -0,0 +1,7 @@
+import app from './app';
+const PORT = process.env.PORT || 3010;
+
+app.listen(PORT, () => {
+  console.log(`Server Running on port ${PORT}`);
+  console.log(`API Testing UI: http://localhost:${PORT}/api/v0/docs/`);
+});
