7fc1704184797d36801693a3aac59ffcd2e92f41
Sanya Murdeshwar <sanya.murdeshwar@gmail.com>
2024-10-22 11:30:15 -0700

    parses first/last name, writes them to console

--- react-login/frontend/src/skillswapper_components/signup/signup.jsx
+++ react-login/frontend/src/skillswapper_components/signup/signup.jsx
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './signup.module.css';
 import signupLogo from '../../images/SignupLoginSkillSwapper.svg';
 import signupText from '../../images/signup.svg';
 
 const SignUpPage = () => {
+  
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    fullName: '',
+    zipCode: '',
+  });
+
   const inputFields = [
     { id: 'email', placeholder: 'Enter Email...', type: 'text' },
     { id: 'password', placeholder: 'Create Password...', type: 'password' },
     { id: 'fullName', placeholder: 'Enter First & Last Name...', type: 'text' },
     { id: 'zipCode', placeholder: 'Enter Zip Code...', type: 'text' },
   ];
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [id]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { fullName } = formData;
+    const [firstName, lastName] = fullName.split(' ');
+    console.log('First Name:', firstName);
+    console.log('Last Name:', lastName); 
+  };
+    
 
   return (
     <main className={styles.signupPage}>
@@ -18,7 +43,7 @@ const SignUpPage = () => {
           <img src={signupLogo} alt="SkillSwapper Signup and Login Logo" className={styles.signupLogo} />
           <img src={signupText} alt="Sign Up" className={styles.signupText} />
         </div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className={styles.formGroup}>
             {inputFields.map((field) => (
               <input
@@ -27,6 +52,8 @@ const SignUpPage = () => {
                 type={field.type}
                 placeholder={field.placeholder}
                 className={styles.inputField}
+                value={formData[field.id]}
+                onChange={handleChange}
               />
             ))}
           </div>
