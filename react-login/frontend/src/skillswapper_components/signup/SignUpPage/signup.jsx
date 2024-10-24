bb9cad8e1a472f189db32de86ebfb8c5d9f65306
Shirin Rokni <shirinrokni@Shirins-MacBook-Pro.local>
2024-10-24 13:32:43 -0700

    made error msgs in signup proper

--- react-login/frontend/src/skillswapper_components/signup/SignUpPage/signup.jsx
+++ react-login/frontend/src/skillswapper_components/signup/SignUpPage/signup.jsx
@@ -8,7 +8,6 @@ import axios from 'axios'
 const SignUpPage = (props) => {
   
     const navigate = useNavigate();
-    // create all values used to maintain page state
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [name, setName] = useState('');
@@ -17,77 +16,70 @@ const SignUpPage = (props) => {
     const [passwordError, setPasswordError] = useState('');
     const [zipError, setZipError] = useState('');
 
-    const onButtonClick = async (e) => {
-        e.preventDefault();
-        setEmailError('');
-        setPasswordError('');
-    
-        let hasError = false;
-    
-        if (email === '') {
+    const validateEmail = (email) => {
+      if (email === '') {
           setEmailError('Email field must not be empty');
-          hasError = true;
-        } else if (!/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+      } else if (!/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
           setEmailError('Please enter a valid email');
-          hasError = true;
-        }
-    
-        if (password === '') {
-          setPasswordError('Password field must not be empty');
-          hasError = true;
-        } else if (password.length < 8) {
-          setPasswordError('Password must be at least 8 characters long');
-          hasError = true;
-        }
-
-        function validZip(zip_code) {
-            const regex = /^\d{5}(-\d{4})?$/;
-            return regex.test(zip_code);
-        }
-
-        if (zip !== '' && !validZip(zip)) {
-            setZipError('Zip Code can only contain numbers');
-            hasError = true;
-        }
-        
-        //zip and name can be allowed empty
-    
-        if (hasError) return;
-
-        const [firstName, lastName] = name.split(' ');
-        const dto = {email: email, password: password, firstName: firstName, lastName: lastName, zip: zip};
-        console.log(dto);
-      
-          const response = await axios.post('http://localhost:3080/api/v0/signup', 
-              dto, 
-              {header: {'Content-Type': 'application/json'}}
-          ).then((res) => {
-              console.log(res);
-              //currently the result of the signup attempt returns just a boolean for whether we succeeded in creating the account, this functionality needs to be expanded. 
-            //   const accessToken = res.data.accessToken;
-            //   const id = res.data.id;
-      
-              props.setLoggedIn(true);
-              props.setEmail(email);
-              navigate('/homepage');
-          }).catch((err) => {
-            console.log(err);
-          })
+      } else {
+          setEmailError('');
+      }
     };
 
+    const validatePassword = (password) => {
+        if (password === '') {
+            setPasswordError('Password field must not be empty');
+        } else if (password.length < 8) {
+            setPasswordError('Password must be at least 8 characters long');
+        } else {
+            setPasswordError('');
+        }
+    };
 
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     const { fullName } = formData;
-//     const [firstName, lastName] = fullName.split(' ');
-//     // console.log('First Name:', firstName);
-//     // console.log('Last Name:', lastName); 
-//     // console.log(formData);
-//     const dto = {email: formData.email, password: formData.password, firstName: firstName, lastName: lastName, zip: formData.zipCode};
-//     console.log(dto);
-//     // axios call here
-//   };
-    
+    const validateZip = (zip_code) => {
+        const regex = /^\d{5}(-\d{4})?$/;
+        if (zip_code !== '' && !regex.test(zip_code)) {
+            setZipError('Zip Code can only contain numbers');
+        } else {
+            setZipError('');
+        }
+    };
+
+    const onButtonClick = async (e) => {
+      e.preventDefault();
+      setEmailError('');
+      setPasswordError('');
+      setZipError('');
+
+      let hasError = false;
+
+      validateEmail(email);
+      validatePassword(password);
+      validateZip(zip);
+
+      // Check if any error exists
+      if (emailError || passwordError || zipError) {
+          hasError = true;
+      }
+
+      if (hasError) return;
+
+      const [firstName, lastName] = name.split(' ');
+      const dto = { email: email, password: password, firstName: firstName, lastName: lastName, zip: zip };
+      console.log(dto);
+
+      const response = await axios.post('http://localhost:3080/api/v0/signup',
+          dto,
+          { header: { 'Content-Type': 'application/json' } }
+      ).then((res) => {
+          console.log(res);
+          props.setLoggedIn(true);
+          props.setEmail(email);
+          navigate('/homepage');
+      }).catch((err) => {
+          console.log(err);
+      });
+  };
 
   return (
     <main className={styles.signupPage}>
@@ -97,105 +89,65 @@ const SignUpPage = (props) => {
           <img src={signupText} alt="Sign Up" className={styles.signupText} />
         </div>
         <form>
-        <div className={styles.formGroup}>
+          <div className={styles.formGroup}>
+            <div className={styles.inputContainer}>
               <input
                 key='email'
                 id='email'
                 type='text'
                 placeholder='Enter Email...'
                 className={styles.inputField}
-                // value={formData['email']}
-                onChange={(ev) => setEmail(ev.target.value)}
+                onChange={(ev) => {
+                  setEmail(ev.target.value);
+                  validateEmail(ev.target.value)
+                }}              
               />
               {emailError && <p className={styles.errorLabel}>{emailError}</p>}
+            </div>
+            <div className={styles.inputContainer}>
               <input
                 key='password'
                 id='password'
                 type='password'
                 placeholder='Create Password...'
                 className={styles.inputField}
-                // value={formData['password']}
-                onChange={(ev) => setPassword(ev.target.value)}
+                onChange={(ev) => {
+                  setPassword(ev.target.value);
+                  validatePassword(ev.target.value);
+                }}
               />
               {passwordError && <p className={styles.errorLabel}>{passwordError}</p>}
+            </div>
+            <div className={styles.inputContainer}>
               <input
                 key='fullName'
                 id='fullName'
                 type='text'
                 placeholder='Enter First & Last Name...'
                 className={styles.inputField}
-                // value={formData['fullName']}
                 onChange={(ev) => setName(ev.target.value)}
               />
+            </div>
+            <div className={styles.inputContainer}>
               <input
                 key='zipCode'
                 id='zipCode'
                 type='text'
                 placeholder='Enter Zip Code...'
                 className={styles.inputField}
-                // value={formData['zipCode']}
-                onChange={(ev) => setZip(ev.target.value)}
-              />
+                onChange={(ev) => {
+                  setZip(ev.target.value);
+                  validateZip(ev.target.value);
+                }}              />
               {zipError && <p className={styles.errorLabel}>{zipError}</p>}
+            </div>
           </div>
           <div className={styles.submitButtonContainer}>
             <button type="button" className={styles.submitButton} onClick={onButtonClick}>
-            Sign Up
-            </button>
-            {/* <button type="submit" className={styles.submitButton}>
               Sign Up
-            </button> */}
+            </button>
           </div>
         </form>
-        {/* <form onSubmit={handleSubmit}>
-          <div className={styles.formGroup}>
-              <input
-                key='email'
-                id='email'
-                type='text'
-                placeholder='Enter Email...'
-                className={styles.inputField}
-                value={formData['email']}
-                onChange={handleChange}
-              />
-              {error}
-              <input
-                key='password'
-                id='password'
-                type='password'
-                placeholder='Create Password...'
-                className={styles.inputField}
-                value={formData['password']}
-                onChange={handleChange}
-              />
-              {error}
-              <input
-                key='fullName'
-                id='fullName'
-                type='text'
-                placeholder='Enter First & Last Name...'
-                className={styles.inputField}
-                value={formData['fullName']}
-                onChange={handleChange}
-              />
-              {error}
-              <input
-                key='zipCode'
-                id='zipCode'
-                type='text'
-                placeholder='Enter Zip Code...'
-                className={styles.inputField}
-                value={formData['zipCode']}
-                onChange={handleChange}
-              />
-              {error}
-          </div>
-          <div className={styles.submitButtonContainer}>
-            <button type="submit" className={styles.submitButton}>
-              Sign Up
-            </button>
-          </div>
-        </form> */}
         <p className={styles.loginLink}>
           Already have an account? <a href="/login">Log in</a>
         </p>
