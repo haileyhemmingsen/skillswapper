d19c5ffff6b50e99e8f574a0f0acfef70ee48e9c
Bradly-H <149456135+Bradly-H@users.noreply.github.com>
2024-10-25 10:18:15 -0700

    signup now works for most inputs. Not thoroughly tested

--- react-login/auth-server/services/account/account.module.service.ts
+++ react-login/auth-server/services/account/account.module.service.ts
@@ -28,7 +28,7 @@ export class LoginService {
         firstname: body.firstname,
         lastname: body.lastname,
         email: body.email,
-        // zip: body.zip,
+        zip: body.zip,
         password: hashedPassword,
         createdAt: new Date(),
         uuid: uuid,
