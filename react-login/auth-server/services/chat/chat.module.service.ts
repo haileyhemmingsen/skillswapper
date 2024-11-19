708320d67ee84b22dd6c699d001b1ee413e50a59
Bradly-H <149456135+Bradly-H@users.noreply.github.com>
2024-11-19 13:52:10 -0800

    send message complete untested, retrieveMessage started

--- react-login/auth-server/services/chat/chat.module.service.ts
+++ react-login/auth-server/services/chat/chat.module.service.ts
@@ -5,6 +5,17 @@ import { db } from '../../firebase'; // Firebase imports
 import { doc, getDoc, setDoc, collection, getDocs, updateDoc, arrayUnion, addDoc } from 'firebase/firestore';
 import { get } from "http";
 
+/*
+send messages,
+upon message sent, 
+
+frontend continually query for new messages when clicked into a specific chat window
+
+upon loading all of the different chats, chats will be ordered by new messages 
+(as in the chat has messages unseen by this user), newest message (time sent)
+ideally mark the chats in frontend somehow to show that they have unread messages
+*/
+
 export class ChatService {
     // if (newMessage.trim()) {
     //   db.collection('chats')
@@ -20,7 +31,6 @@ export class ChatService {
     // }
     public async sendMessage(message: Message, user_id: string): Promise<boolean | undefined> {
         // we are sending a message here
-        const chatsSnapShot = await getDocs(collection(db, 'chats'));
         if(message.chatID !== undefined) {
             // then chat already exists
             console.log(message.chatID);
@@ -34,16 +44,42 @@ export class ChatService {
 
                 // here is how to add the message:
                 // https://firebase.google.com/docs/reference/js/v8/firebase.firestore.CollectionReference#add
-                await addDoc(collection(chat_ref, 'messages'), {
+                const message_uuid = uuidv4();
+                await setDoc(doc(chat_ref, 'messages', message_uuid), {
+                    id: message_uuid,
                     senderID: message.sender,
                     message: message.message,
                     timestamp: message.timestamp
                 });
+                // await addDoc(collection(chat_ref, 'messages'), {
+                //     senderID: message.sender,
+                //     message: message.message,
+                //     timestamp: message.timestamp
+                // });
             }
         }
         else {
             // need to create a new chat
             const chat_uuid = uuidv4();
+
+            await setDoc(doc(db, 'chats', chat_uuid), {
+                chat_id: chat_uuid,
+                user_1: message.sender,
+                user_2: message.receiver,
+            });
+            const chat_ref = doc(db, 'chats', chat_uuid);
+            // create new collection
+            const message_ref = collection(chat_ref, 'messages');
+            const message_uuid = uuidv4(); 
+            await setDoc(doc(message_ref, 'messages', message_uuid), {
+                id: message_uuid,
+                senderID: message.sender,
+                message: message.message,
+                timestamp: message.timestamp
+            })
+            
+            // upon creating new chat, need to add sender ID to chat doc, receiver ID to chat doc, 
+            // the messages collection, and then add the message to the messages collection
         }
         return true;
     }
@@ -56,7 +92,15 @@ export class ChatService {
     //   .onSnapshot((snapshot) => {
     //     setMessages(snapshot.docs.map((doc) => doc.data()));
     //   });
-    public async unsubscribe(sender: string, receiver: string): Promise<boolean | undefined> {
+    // public async unsubscribe(sender: string, receiver: string): Promise<boolean | undefined> {
+    //     return true;
+    // }
+
+    public async retrieveMessage(receiver: string, id: string): Promise <boolean | undefined> {
+        
+
+
         return true;
     }
+
 }
